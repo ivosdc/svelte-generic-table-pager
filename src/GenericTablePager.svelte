521808be450fe894d49d55560708cd4877131e86
ivosdc <ibozic@mail.de>
2020-10-17 22:22:23 +0200

    git subtree CrudTable for precompiling components in jest

--- src/GenericTablePager.svelte
+++ src/GenericTablePager.svelte
@@ -3,7 +3,7 @@
     import {createEventDispatcher, onMount, beforeUpdate, afterUpdate} from 'svelte';
     import {GenericTablePagerService} from "./GenericTablePagerService";
     import {iconLeft, iconRight} from './SvgIcons'
-    import SvelteGenericCrudTable from 'svelte-generic-crud-table';
+    import SvelteGenericCrudTable from './CrudTable/src/SvelteGenericCrudTable.svelte';
 
     /* istanbul ignore next line */
     export let shadowed = false;
