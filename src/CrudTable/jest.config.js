8be1e08a7c9c1e1fd6f64e84c86658909f45d31b
ivosdc <ibozic@mail.de>
2020-10-18 16:38:46 +0200

    remove CrudTable

--- src/CrudTable/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-        '^.+\\.svelte$': 'svelte-jester',
-        '^.+\\.js$': 'babel-jest',
-        "^.+\\.svg?$": "svelte-jester"
-    },
-    moduleFileExtensions: ['js', 'svelte'],
-    resolver: 'jest-svelte-resolver'
-}
